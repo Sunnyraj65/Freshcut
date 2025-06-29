d61e2fa9932f19b2d4fb5c9813268d202ab8e8e2
Sunnyraj65 <162629289+Sunnyraj65@users.noreply.github.com>
2025-06-29 15:32:32 +0530

    Fix Network Error with API Proxy Configuration

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "/api",
 });
 
 api.interceptors.request.use((config) => {
